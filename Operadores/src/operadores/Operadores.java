a1148d1e330022357bed1cbcc41c00864b38732c
SimonRiquelme <simon.riquelmer@gmail.com>
2015-11-09 02:09:20 -0300

    Subiendo Protecto

---
+++ Operadores/src/operadores/Operadores.java
@@ -0,0 +1,35 @@
+
+package operadores;
+
+
+public class Operadores {
+
+
+    public static void main(String[] args) {
+        /*
+        aritmeticos(+,-,*,/)
+         logico >,<,>=.<=,!=
+         incremento ++
+         decremento --
+        
+        */
+                
+        int x=4;
+        int y=8;
+        double resultado;
+        
+            resultado=x+y;
+            System.out.println("El resultado de la suma es: "+resultado);
+            
+            resultado=x-y;
+            System.out.println("El resultado de la resta es: "+resultado);
+            
+            resultado=x*y;
+            System.out.println("El resultado de la multiplicacion es: "+resultado);
+            
+            resultado=y/3;
+            System.out.println("El resultado de la Division es: "+resultado);
+            
+    }
+    
+}
